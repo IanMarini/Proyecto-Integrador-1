66f8e0cb0a088f3dd7dde162c26e3cdc53b90a9c
Ian Marini <135738365+IanMarini@users.noreply.github.com>
2024-06-08 19:41:49 -0300

    Update libros.py

--- Programación/Gestion_Biblioteca/libros.py
+++ Programación/Gestion_Biblioteca/libros.py
@@ -1,3 +1,5 @@
+libros = []  # Lista para almacenar libros
+
 def mostrar_menu_libros():
     print("")
     print('1. Agregar Libro')
@@ -9,16 +11,55 @@ def mostrar_menu_libros():
     opcion_menu_libros = input('Por favor, elija la opción deseada: ')
 
     if opcion_menu_libros == '1':
-        print('Agregar Libro seleccionado')
+        agregar_libro()
     elif opcion_menu_libros == '2':
-        print('Ver Libros seleccionado')
+        ver_libros()
     elif opcion_menu_libros == '3':
-        print('Actualizar Libro seleccionado')
+        actualizar_libro()
     elif opcion_menu_libros == '4':
-        print('Eliminar Libro seleccionado')
+        eliminar_libro()
     elif opcion_menu_libros == '5':
-        from index import menu_principal
         menu_principal()
     else:
         print('Por favor, ingrese una opción válida')
         mostrar_menu_libros()
+
+def agregar_libro():
+    titulo = input('Ingrese el título del libro: ')
+    autor = input('Ingrese el autor del libro: ')
+    ano = input('Ingrese el año de publicación del libro: ')
+    isbn = input('Ingrese el ISBN del libro: ')
+    libros.append({'titulo': titulo, 'autor': autor, 'año': ano, 'ISBN': isbn})
+    print(f'Libro agregado exitosamente: {titulo} por {autor}')
+    mostrar_menu_libros()
+
+def ver_libros():
+    print('Lista de libros...')
+    for libro in libros:
+        print(f"Título: {libro['titulo']}, Autor: {libro['autor']}, Año: {libro['año']}, ISBN: {libro['ISBN']}")
+    mostrar_menu_libros()
+
+def actualizar_libro():
+    isbn = input('Ingrese el ISBN del libro a actualizar: ')
+    for libro in libros:
+        if libro['ISBN'] == isbn:
+            nuevo_titulo = input('Ingrese el nuevo título del libro: ')
+            nuevo_autor = input('Ingrese el nuevo autor del libro: ')
+            nuevo_ano = input('Ingrese el nuevo año de publicación del libro: ')
+            libro['titulo'] = nuevo_titulo
+            libro['autor'] = nuevo_autor
+            libro['año'] = nuevo_ano
+            print('Libro actualizado exitosamente')
+            mostrar_menu_libros()
+    print('No se encontró ningún libro con ese ISBN')
+    mostrar_menu_libros()
+
+def eliminar_libro():
+    isbn = input('Ingrese el ISBN del libro a eliminar: ')
+    for libro in libros:
+        if libro['ISBN'] == isbn:
+            libros.remove(libro)
+            print('Libro eliminado exitosamente')
+            mostrar_menu_libros()
+    print('No se encontró ningún libro con ese ISBN')
+    mostrar_menu_libros()
