d4c8ece317923bcc290451893ead837b4cc1130e
Ian Marini <135738365+IanMarini@users.noreply.github.com>
2024-06-11 14:51:23 -0300

    Update usuarios.py

--- Programación/Gestion_Biblioteca/usuarios.py
+++ Programación/Gestion_Biblioteca/usuarios.py
@@ -1,5 +1,30 @@
 usuarios = []  # Lista para almacenar usuarios
 
+def mostrar_menu_usuarios():
+    print("")
+    print('1. Agregar Usuario')
+    print('2. Ver Usuarios')
+    print('3. Actualizar Usuario')
+    print('4. Eliminar Usuario')
+    print('5. Volver al menú principal')
+    print("")
+    opcion_menu_usuarios = input('Por favor, elija la opción deseada: ')
+
+    if opcion_menu_usuarios == '1':
+        agregar_usuario()
+    elif opcion_menu_usuarios == '2':
+        ver_usuarios()
+    elif opcion_menu_usuarios == '3':
+        actualizar_usuario()
+    elif opcion_menu_usuarios == '4':
+        eliminar_usuario()
+    elif opcion_menu_usuarios == '5':
+        menu_principal()
+    else:
+        print('Por favor, ingrese una opción válida')
+        mostrar_menu_usuarios()
+
+
 def agregar_usuario():
     nombre = input('Ingrese el nombre del usuario: ')
     apellido = input('Ingrese el apellido del usuario: ')
