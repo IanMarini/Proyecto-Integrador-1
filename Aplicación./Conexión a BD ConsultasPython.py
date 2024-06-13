c1dd48c24104ec73d5e86fc58ecf45f4a023eba9
Ian Marini <135738365+IanMarini@users.noreply.github.com>
2024-06-13 18:34:25 -0300

    Update Conexión a BD ConsultasPython.py

--- Aplicación./Conexión a BD ConsultasPython.py
+++ Aplicación./Conexión a BD ConsultasPython.py
@@ -8,14 +8,15 @@ def get_db_connection():
         database='Gestion_Biblioteca'
     )
     return connection
-print("===========================================")
-print("SE REALIZO LA CONEXCION DE MANERA EXITOSA")
-print("===========================================")
+print("===============================================")
+print("---SE REALIZO LA CONEXCION DE MANERA EXITOSA---")
+print("===============================================")
 
 
-print("======================")
+
+print("=====================")
 print("FUNCIONES PARA LIBROS")
-print("======================")
+print("=====================")
 # Funciones CRUD para Libros.
 def add_book(titulo, autor, isbn, editorial, anio_publicacion, disponibilidad):
     connection = get_db_connection()
@@ -27,7 +28,10 @@ def add_book(titulo, autor, isbn, editorial, anio_publicacion, disponibilidad):
     connection.commit()
     cursor.close()
     connection.close()
-# Funcion para traer y mostrar los datos de la Tabla Libros.
+
+
+
+# Funcion para TRAER Y MOSTRAR LOS DATOS DE LA TABLA LIBROS.
 def get_books():
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -37,7 +41,9 @@ def get_books():
     connection.close()
     return result
 
-# Funcion para actualizar libros.
+
+
+# Funcion para ACTUALIZAR LIBROS.
 def update_book(id, disponibilidad):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -46,7 +52,9 @@ def update_book(id, disponibilidad):
     cursor.close()
     connection.close()
 
-# Funcion eliminar libros.
+
+
+# Funcion para ELIMINAR LIBROS.
 def delete_book(id):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -54,6 +62,8 @@ def delete_book(id):
     connection.commit()
     cursor.close()
     connection.close()
+
+
 
 print("=======================")
 print("FUNCIONES PARA USUARIOS")
@@ -70,7 +80,7 @@ def add_user(nombre, direccion, telefono, email):
     cursor.close()
     connection.close()
     
-# Funcion para traer y mostrar los datos de la Tabla Usuarios.
+# Funcion para TRAER Y MOSTRAR LOS DATOS DE LA TABLA USUARIOS.
 def get_users():
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -80,7 +90,7 @@ def get_users():
     connection.close()
     return result
 
-# Funcion actualizar usuario.
+# Funcion para ACTUALIZAR USUARIOS.
 def update_user(id, telefono):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -89,23 +99,25 @@ def update_user(id, telefono):
     cursor.close()
     connection.close()
 
-# Funcion eliminar usuario.
+# Funcion para ELIMINAR USUARIOS.
 def delete_user_and_loans(user_id):
     connection = get_db_connection()
     cursor = connection.cursor()
     
-    # Eliminar préstamos asociados al usuario
+    # Eliminar préstamos asociados al usuario.
     cursor.execute("DELETE FROM Prestamos WHERE ID_Usuario = %s", (user_id,))
     connection.commit()
     print(f"Préstamos asociados al usuario con ID {user_id} eliminados")
     
-    # Eliminar el usuario
+    # Eliminar el usuario.
     cursor.execute("DELETE FROM Usuarios WHERE ID = %s", (user_id,))
     connection.commit()
     print(f"Usuario con ID {user_id} eliminado")
     
     cursor.close()
     connection.close()
+
+
 
 print("========================")
 print("FUNCIONES PARA PRESTAMOS")
@@ -122,7 +134,7 @@ def add_loan(id_libro, id_usuario, fecha_prestamo, fecha_devolucion=None):
     cursor.close()
     connection.close()
 
-# Funcion para traer y mostrar los datos de la Tabla Prestamos.
+# Funcion para TRAER Y MOSTRAR LOS DATOS DE LA TABLA PRESTAMOS.
 def get_loans():
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -137,7 +149,7 @@ def get_loans():
     connection.close()
     return result
 
-# Funcion actualizar prestamos.
+# Funcion para ACTUALIZAR PRESTAMOS.
 def update_loan(id, fecha_devolucion):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -146,7 +158,7 @@ def update_loan(id, fecha_devolucion):
     cursor.close()
     connection.close()
 
-# Funcion eliminar prestamos.
+# Funcion para ELIMINAR PRESTAMOS.
 def delete_loan(id):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -159,6 +171,7 @@ print("=========================")
 print("FUNCIONES PARA CATEGORIAS")
 print("=========================")
 # Funciones CRUD para Categorias
+# Funcion para ACTUALIZAR PRESTAMOS.
 def add_category(nombre):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -168,6 +181,9 @@ def add_category(nombre):
     connection.close()
     print(f"Categoría agregada: {nombre}")
 
+
+
+# Funcion para TRAER Y MOSTRAR LOS DATOS DE LA TABLA PRESTAMOS.
 def get_categories():
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -178,6 +194,9 @@ def get_categories():
     print("Obteniendo todas las categorías")
     return result
 
+
+
+# Funcion para ELIMINAR PRESTAMOS.
 def delete_category(id):
     connection = get_db_connection()
     cursor = connection.cursor()
@@ -187,60 +206,106 @@ def delete_category(id):
     connection.close()
     print(f"Categoría con ID {id} eliminada")
 
-
-
-
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
+
+# A continuación, añadimos un nuevo libro a la base de datos con la función add_book. 
+# Esta función inserta un nuevo registro en la tabla Libros.
     # Agregar un libro
     add_book('El Alquimista', 'Paulo Coelho', '978-0061122415', 'HarperOne', 1993, True)
+    print("SE AGREGO UN LIBRO") 
 
+#La función get_books recupera todos los registros de la tabla Libros y los almacena en la variable libros. 
+# Luego, el bucle for itera sobre cada libro y lo imprime, mostrando todos los libros disponibles en la base de datos.
     # Obtener todos los libros
     libros = get_books()
     for libro in libros:
         print(libro)
+        print("MUESTRA LOS DATOS DE LA TABLA LIBROS")
 
+# La función update_book actualiza el campo Disponibilidad del libro con ID igual a 1. 
+# En este caso, establece la disponibilidad como True, indicando que el libro está disponible.
     # Actualizar la disponibilidad de un libro
-    update_book(1, False)
+    update_book(1, True)
+    print("ACTUALIZO UN LIBRO")
 
+# La función delete_book elimina el libro con ID igual a 11 de la tabla Libros, 
+# eliminando así el registro del libro de la base de datos.
     # Eliminar un libro
-    delete_book(2)
+    delete_book(11)
+    print("ELIMINO UN LIBRO REGISTRADO")
 
+# Agregar un nuevo usuario
+# Primero, creamos un nuevo usuario en la base de datos utilizando la función add_user. 
+# Esta función inserta un nuevo registro en la tabla Usuarios con los datos proporcionados.
     # Agregar un usuario
-    add_user('Pedro Gonzales', 'Calle Flores 789, Ciudad', '111-2222', 'pedro.gonzales@example.com')
+    add_user('Alan Gonzales', 'Calle Paco 789, Ciudad', '123-4512', 'Balan.gonzales@example.com')
+    print("SE AGREGO UN USUARIO")
 
+# La función get_users recupera todos los registros de la tabla Usuarios y los almacena en la variable usuarios. 
+# Luego, el bucle for itera sobre cada usuario y lo imprime, mostrando todos los usuarios registrados en la base de datos.
     # Obtener todos los usuarios
     usuarios = get_users()
     for usuario in usuarios:
         print(usuario)
+        print("MUESTRA LOS DATOS DE LA TABLA USUARIOS")
 
+# La función update_user actualiza el campo Telefono del usuario con ID igual a 3. 
+# En este caso, establece el nuevo teléfono como "351-8623638".
     # Actualizar el teléfono de un usuario
-    update_user(1, '351-6623038')
+    update_user(3, '351-8623638')
+    print("ACTUALIZO UN USUARIO")
 
+# La función delete_user_and_loans elimina el usuario con ID igual a 5 de la tabla Usuarios junto con todos sus préstamos asociados. 
+# Esto asegura que no queden registros huérfanos en la base de datos.
     # Eliminar un usuario
-    delete_user_and_loans(2)
+    delete_user_and_loans(5)
+    print("ELIMINO UN USUARIO REGISTRADO")
 
+# La función add_loan inserta un nuevo registro en la tabla Prestamos con los datos proporcionados:
     # Agregar un préstamo
     add_loan(1, 1, '2024-06-01')
+    print("SE AGRGO UN PRÉSTAMO")
 
+#La función get_loans recupera todos los registros de la tabla Prestamos y los almacena en la variable prestamos.
+# Luego, el bucle for itera sobre cada préstamo y lo imprime, mostrando todos los préstamos registrados en la base de datos.
     # Obtener todos los préstamos
     prestamos = get_loans()
     for prestamo in prestamos:
         print(prestamo)
+        print("MUESTRA LOS DATOS DE LA TABLA PRÉSTAMOS")
 
+#La función update_loan actualiza el campo Fecha_Devolucion del préstamo con ID igual a 1. 
+# En este caso, establece la nueva fecha de devolución como '2024-06-10'.
     # Actualizar la fecha de devolución de un préstamo
     update_loan(1, '2024-06-10')
+    print("ACTUALIZO LA FECHA DE DEVOLUCIÓN")
 
+#La función delete_loan elimina el préstamo con ID igual a 2 de la tabla Prestamos, 
+# eliminando así el registro del préstamo de la base de datos.
     # Eliminar un préstamo
     delete_loan(2)
+    print("ELIMINO UN PRESTAMO REGISTRADO")
 
+# La función add_category inserta un nuevo registro en la tabla Categorias con el nombre proporcionado, 
+# en este caso, "Ficción".
     # Agregar una categoría
     add_category('Ficción')
+    print("SE REGISTRO UNA CATEGORIA NUEVA")
 
+# La función get_categories recupera todos los registros de la tabla Categorias y los almacena en la variable categorias. 
+# Luego, el bucle for itera sobre cada categoría y la imprime, mostrando todas las categorías registradas en la base de datos.
     # Obtener todas las categorías
     categorias = get_categories()
     for categoria in categorias:
         print(categoria)
+    print("MUESTRA LOS DATOS DE LA TABLA CATEGORIA")
 
+# La función delete_category elimina la categoría con ID igual a 2 de la tabla Categorias, 
+# eliminando así el registro de la categoría de la base de datos.
     # Eliminar una categoría
     delete_category(2)
+    print("ELIMINO UNA CATEGORIA")
